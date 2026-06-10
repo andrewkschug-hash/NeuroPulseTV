7dfe806dab72084c429dd700fce852666cfefd89
andrewkschug-hash <andrewkschug@gmail.com>
2026-06-10 16:39:44 -0400

    554

--- app/src/main/java/com/neuropulse/tv/ui/screen/onboarding/OnboardingComponents.kt
+++ app/src/main/java/com/neuropulse/tv/ui/screen/onboarding/OnboardingComponents.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.focus.FocusRequester
